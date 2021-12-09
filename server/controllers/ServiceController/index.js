93f084a0fa12c26a3190e2514877d05986a3cf7f
Vitor Brangioni <vitorh.brangioni@gmail.com>
2021-12-09 18:53:09 -0300

    Refactoring code

---
+++ server/controllers/ServiceController/index.js
@@ -0,0 +1,66 @@
+const DatabaseListener = require('../../services/DatabaseListener');
+const Service = require('../../models/Service');
+
+const helperToServiceMapStatus = {
+  1: 'servicesScheduled',
+  2: 'servicesPendding',
+  3: 'servicesInProgress'
+};
+
+class ServiceController {
+
+  static databaseChanges = new DatabaseListener();
+  static servicesDto = {
+    servicesInProgress: [],
+    servicesPendding: [],
+    servicesScheduled: []
+  };
+
+  constructor() {
+    throw new Error('It is not instantiable')
+  }
+
+  static async fetchServicesFromDatabaseAndSend(socket) {
+    const servicesFound = await Service.findAll();
+
+    servicesFound.map(service => {
+      const serviceKey = helperToServiceMapStatus[service.status_id];
+
+      ServiceController.servicesDto[serviceKey].push(service);
+    });
+
+    socket.onConnection((_socket) => {
+      console.log(`Socket connection `, _socket.id);
+      socket.sendToAllSubscribers('receivedMessage', ServiceController.servicesDto);
+    })
+  }
+
+  static listenAndSendServicesStatus(socket) {
+
+      ServiceController.databaseChanges.onUpdate((e) => {
+        ServiceController._managementState(e.old_record, e.record);
+        socket.sendToAllSubscribers('receivedMessage', ServiceController.servicesDto);
+      }, 'public:service:status_id=eq.1');
+
+      ServiceController.databaseChanges.onUpdate((e) => {
+        ServiceController._managementState(e.old_record, e.record);
+        socket.sendToAllSubscribers('receivedMessage', ServiceController.servicesDto);
+      }, 'public:service:status_id=eq.2');
+
+      ServiceController.databaseChanges.onUpdate((e) => {
+        ServiceController._managementState(e.old_record, e.record);
+        socket.sendToAllSubscribers('receivedMessage', ServiceController.servicesDto);
+      }, 'public:service:status_id=eq.3');
+    }
+
+  static _managementState(oldRecord, newRecord) {
+      const oldService = helperToServiceMapStatus[oldRecord.status_id];
+      const newService = helperToServiceMapStatus[newRecord.status_id];
+
+      ServiceController.servicesDto[oldService] = ServiceController.servicesDto[oldService].filter(_service => (_service.id != oldRecord.id));
+      ServiceController.servicesDto[newService].push(newRecord);
+    }
+
+}
+
+module.exports = ServiceController;
--- server/src/server.js
+++ server/src/server.js
@@ -4,8 +4,7 @@ const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const Socket = require('../services/Socket');
-const DatabaseListener = require('../services/DatabaseListener');
-const Service = require('../models/Service');
+const ServiceController = require('../controllers/ServiceController');
 
 app.use(express.static(path.join(__dirname, '../../public')));
 app.set('views', path.join(__dirname, '../../public'));
@@ -16,72 +15,12 @@ app.use('/public', (req, res) => {
   res.render('index.html');
 });
 
-const socket = new Socket(server);
-const databaseChanges = new DatabaseListener();
+(async () => {
+  const socket = new Socket(server);
 
-const servicesDto = {
-  servicesInProgress: [],
-  servicesPendding: [],
-  servicesScheduled: []
-};
-
-const servicesMapToStatus = {
-  1: 'servicesScheduled',
-  2: 'servicesPendding',
-  3: 'servicesInProgress'
-};
-
-
-( async () => {
-
-  const servicesFound = await Service.findAll();
-  console.log('servicesFound' , servicesFound);
-
-
-  servicesFound.map(service => {
-    switch (service.status_id) {
-      case '1': 
-      servicesDto.servicesScheduled.push(service);
-        break;
-
-      case '2': 
-      servicesDto.servicesPendding.push(service);
-        break;
-
-      case '3': 
-      servicesDto.servicesInProgress.push(service);
-        break;
-    }
-  });
-
-  socket.onConnection((_socket) => {
-    console.log(`_socket conn `, _socket.id);
-    socket.sendToAllSubscribers('receivedMessage', servicesDto);
-  })
-  
+  ServiceController.fetchServicesFromDatabaseAndSend(socket);
+  ServiceController.listenAndSendServicesStatus(socket);
 })();
 
-const managementState = (oldRecord, newRecord) => {
-  const oldService = servicesMapToStatus[oldRecord.status_id];
-  const newService = servicesMapToStatus[newRecord.status_id];
-
-  servicesDto[oldService] = servicesDto[oldService].filter(_service => (_service.id != oldRecord.id));
-  servicesDto[newService].push(e.newRecord);
-}
-
-databaseChanges.onUpdate((e) => {
-  managementState(e.old_record, e.record);
-  socket.sendToAllSubscribers('receivedMessage', servicesDto);
-}, 'public:service:status_id=eq.1');
-
-databaseChanges.onUpdate((e) => {
-  managementState(e.old_record, e.record);
-  socket.sendToAllSubscribers('receivedMessage', servicesDto);
-}, 'public:service:status_id=eq.2');
-
-databaseChanges.onUpdate((e) => {
-  managementState(e.old_record, e.record);
-  socket.sendToAllSubscribers('receivedMessage', servicesDto);
-}, 'public:service:status_id=eq.3');
 
 server.listen(8181);
